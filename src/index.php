556ba27d61cbe0209d46abe332bf120770dc2eec
Dax <157641@student.horizoncollege.nl>
2021-09-22 12:05:21 +0200

    Uitwerking van de exercise

--- src/index.php
+++ src/index.php
@@ -1,6 +1,15 @@
 <?php
 
-file_exists();
-method_exists();
+require '../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('./views');
+$twig = new \Twig\Environment($loader);
+
+$name = "Daxx";
+
+echo $twig->render('index.html', ['name' => $name]);
+
+
+
 
 ?>
