0234dc2620b22cd51a05049e434a1187508e403f
Dax <157641@student.horizoncollege.nl>
2021-10-13 13:54:37 +0200

    Uitwerking van de exercise

--- src/controllers/HomeController.class.php
+++ src/controllers/HomeController.class.php
@@ -34,14 +34,6 @@ class HomeController
         $convertedBooks = R::convertToBeans("book", $books);
         $template = $this->twig->load('home.html');
         echo $template->render(["books" => $convertedBooks]);
-        // foreach ($convertedBooks as $boek) {
-        //     echo "<tr>";
-        //     echo  "<td>" . $boek["title"] .  "</td>";
-        //     echo "<td>" . $boek["author_id"] .  "</td>";
-        //     echo "<td>" . $boek["publisher_id"] .  "</td>";
-        //     echo "</tr>";
-        // }
-          
     }
 }
 ?>
