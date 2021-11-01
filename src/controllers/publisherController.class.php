e20552ef60ca12e43bf41a301d16ce49e8d0b480
Dax <157641@student.horizoncollege.nl>
2021-11-01 10:01:03 +0100

    Uitwerking van de exercise

--- src/controllers/publisherController.class.php
+++ src/controllers/publisherController.class.php
@@ -6,7 +6,7 @@
     
     use \RedBeanPHP\R as R;
 
-    class publisherController
+class publisherController
     {
         private $twig;
 
@@ -14,33 +14,34 @@
             $this->twig = $twig;
     }
 
-        public function add()
-        {
-            $validate = new UserService();
-            $validate->validateLoggedIn();
-            $template = $this->twig->load('index.html');
-            echo $template->render();
-        }
+    public function add()
+    {
+        $validate = new UserService();
+        $validate->validateLoggedIn();
+        $template = $this->twig->load('index.html');
+        echo $template->render();
+    }
         
-        public function addPOST()
-        {
-            $name = $_POST["text"];
-            $query = "INSERT INTO publishers (name) VALUES (\"$name\")";
-            R::exec($query);
-            $id = R::getInsertID();
-            header("Location:./index");
-        }
-        public function index()
-        {
-            $validate = new UserService();
-            $validate->validateLoggedIn();
-            $books = R::getAll("SELECT * FROM publishers");
-            $publish = R::convertToBeans("publishers", $books);
-            $template = $this->twig->load('publish.html');
-            echo $template->render(["publishers" => $publish]);
-            foreach ($publish as $ding) {
-                echo $ding["name"] . "<br>";
-            }
+    public function addPOST()
+    {
+        $name = $_POST["text"];
+        $query = "INSERT INTO publishers (name) VALUES (\"$name\")";
+        R::exec($query);
+        $id = R::getInsertID();
+        header("Location:./index");
+    }
+        
+    public function index()
+    {
+        $validate = new UserService();
+        $validate->validateLoggedIn();
+        $books = R::getAll("SELECT * FROM publishers");
+        $publish = R::convertToBeans("publishers", $books);
+        $template = $this->twig->load('publish.html');
+        echo $template->render(["publishers" => $publish]);
+        foreach ($publish as $ding) {
+            echo $ding["name"] . "<br>";
         }
     }
-    ?>
+}
+?>
